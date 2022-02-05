fb535064f40aa1e18efa0e107e4de1e056f8d4d8
voolga <olga.volotovich@mail.ru>
2022-02-05 21:16:55 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,8 @@
+let burger = document.querySelector('.burger');
+let mobileMenu = document.querySelector('.nav-list');
+
+burger.onclick = function() {
+  mobileMenu.classList.toggle('nav-list_active');
+};
+
+
